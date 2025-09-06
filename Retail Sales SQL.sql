03421dc98a0d9d945efc8f7fb925dcbe985b1de3
Dominic Rinaldi <rinaldi.dominic@yahoo.com>
2025-09-05 19:29:10 -0600

    Add files via upload

---
+++ Retail Sales SQL.sql
@@ -0,0 +1,156 @@
+-- Create Database Retail Sales Project
+CREATE DATABASE RetailSales;
+
+-- Drop Table in case of requery
+DROP TABLE IF EXISTS retail_sales;
+-- Create Table
+CREATE TABLE retail_sales
+	(
+	transactions_id INT PRIMARY KEY,	
+    sale_date DATE,	
+    sale_time TIME,
+	customer_id INT,
+	gender VARCHAR(10),
+	age INT,
+	category VARCHAR(20),
+	quantity INT,
+	price_per_unit FLOAT,
+	cogs FLOAT,
+	total_sale FLOAT
+    ); 
+
+-- Check the Data
+SELECT *
+FROM retail_sales
+LIMIT 10;
+
+-- Ensuring correct # rows (1987)
+SELECT COUNT(*) 
+FROM retail_sales
+LIMIT 10;
+
+-- Clean Data
+-- No Nulls so no cleaning needed
+SELECT *
+FROM retail_sales
+WHERE 
+	transactions_id IS NULL
+    OR sale_date IS NULL
+    OR sale_time IS NULL
+    OR customer_id IS NULL
+    OR gender IS NULL
+    OR age IS NULL
+    OR category IS NULL
+    OR quantity IS NULL
+    OR price_per_unit IS NULL
+    OR cogs IS NULL
+    OR total_sale IS NULL;
+    
+-- Data Exploration
+
+	-- How many sales were there?
+	SELECT 
+		COUNT(*) AS total_sales
+	FROM retail_sales;
+
+	-- How many customers are there?
+	SELECT
+		COUNT(DISTINCT(customer_id)) AS customer_total
+	FROM retail_sales;
+
+	-- How many categories are there?
+	SELECT
+		COUNT(DISTINCT(category)) AS customer_total
+	FROM retail_sales; 
+
+-- Analysis Questions
+	-- 1. Find all sales made on November 5, 2022
+	SELECT *
+	FROM retail_sales
+	WHERE sale_date = '2022-11-05';
+
+	-- 2. Find all transactions where the category is "clothing", the quantity is at least 4, and the dates are during November 2022
+    SELECT *
+    FROM retail_sales
+    WHERE
+		category = 'Clothing'
+        AND quantity >= 4
+        AND YEAR(sale_date) = '2022'
+        AND MONTH(sale_date) = '11';
+        
+	-- 3. Calculate the total sales for each category
+    SELECT
+		category,
+        sum(total_sale) AS net_sale,
+        count(*) AS total_orders
+	FROM retail_sales
+    GROUP BY category;
+    
+    -- 4. Find the average age of customers who purchased items from the "beauty" category\
+    SELECT
+		category,
+        ROUND(avg(age), 2)	 AS avg_age
+	FROM retail_sales
+    WHERE category = 'Beauty';
+    
+    
+    -- 5. Find all transactions where the total sale is greater than 1000
+    SELECT *
+    FROM retail_sales
+    WHERE total_sale > 1000;
+    
+    -- 6. Find the total number of transactions made by each gender in each category
+    SELECT 
+        category,
+        gender,
+        COUNT(DISTINCT(transactions_id)) AS number_of_transactions
+	FROM retail_sales
+    GROUP BY category, gender;
+    
+    -- 7. Calculate the average sale for each month. Use this to find the best-selling month each year.
+    SELECT * FROM 
+    (
+    SELECT
+		YEAR(sale_date) AS year,
+        MONTH(sale_date) AS month,
+        ROUND(avg(total_sale),2) as avg_sales,
+        RANK() OVER(PARTITION BY YEAR(sale_date) ORDER BY ROUND(avg(total_sale),2) DESC) as 'rank'
+	FROM retail_sales
+    GROUP BY year, month
+    ORDER BY year, 3 DESC
+    ) as t1
+    WHERE t1.rank = '1';
+    
+    -- 8. Find the top 5 customers based on highest total sales
+    SELECT
+		customer_id,
+        sum(total_sale) as net_sales
+	FROM retail_sales
+    GROUP BY customer_id
+    ORDER BY net_sales DESC
+	LIMIT 5;
+	
+    -- 9. Find the number of unique customers who purchased items from each category
+    SELECT 
+		category,
+        COUNT(DISTINCT(customer_id)) as customers
+	FROM retail_sales
+    GROUP BY category;
+    
+    -- 10. Create number of orders per shift (Morning - before 12pm, Afternoon - 12pm-5pm, Evening - after 5pm)
+   WITH hourly_sales
+   AS
+   (
+   SELECT *,
+		CASE
+			WHEN HOUR(sale_time) < 12 THEN 'Morning'
+            WHEN HOUR(sale_time) > 17 THEN 'Evening'
+            ELSE 'Afternoon'
+		END AS shift
+	FROM retail_sales
+    )
+    SELECT 
+		shift,
+        COUNT(DISTINCT(transactions_id)) as orders
+    FROM hourly_sales
+    GROUP BY shift
